d4bce4bac9879d67e28f3cf427d3b16588e70061
Oleksandr <galexing@gmail.com>
2021-02-09 13:15:10 +0100

    complite task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 import './styles.css';
+
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stopBtnRef = document.querySelector('button[data-action="stop"]');
+
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+const randomBackgroundColor = element => {
+  element.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`;
+};
+
+startBtnRef.addEventListener('click', function startProcess() {
+  startBtnRef.removeEventListener('click', startProcess);
+  startBtnRef.disabled = true;
+  const intervalColor = setInterval(() => {
+    randomBackgroundColor(document.body);
+  }, 1000);
+
+  stopBtnRef.addEventListener('click', function stopProcess() {
+    clearInterval(intervalColor);
+    startBtnRef.disabled = false;
+    startBtnRef.addEventListener('click', startProcess);
+    stopBtnRef.removeEventListener('click', stopProcess);
+  });
+});
